c02a61e7eb1f5827a9ffb444d7c4a0dc7da8f906
Kelly Condori <kellycondoricondori@gmail.com>
2024-12-23 22:17:35 -0500

    Método constructor, como se invova y se ejecuta

--- sayajin.php
+++ sayajin.php
@@ -1,10 +1,16 @@
 <?php
 class Sayajin{
-    public string $nombre="Goku";
-    public int $nivel_pelea=1000;
+    public string $nombre;
+    public int $nivel_pelea;
 
-    public function Saludar(){
-        return "Hola soy ". "$this ->nombre";
+    public function __construct(string $nombre, int $nivel_pelea){
+        $this->nombre=$nombre;
+        $this->nivel_pelea=$nivel_pelea;
+    }
+    
+
+    public function Saludar(string $texto=" Hola soy "){
+        return $texto. $this ->nombre;
 
     }
 
@@ -15,10 +21,11 @@ class Sayajin{
 
 }
 
-$goku=new Sayajin();
-echo $goku->nombre;
+$goku=new Sayajin("Goku", 1000);
 echo $goku->Saludar();
+echo "<br><br>";
 
-$vegueta = new Sayajin();
+$vegueta = new Sayajin("Vegeta", 950);
+echo $vegueta->Saludar("Mi nombre es ");
 
 ?>
